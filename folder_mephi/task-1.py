2cec527d896f7ffc2c8c4f01ef9717af176b1fa6
Anastasia <nastya_romanova-2019@bk.ru>
2024-10-20 17:09:42 +0300

    task 1 completed

--- folder_mephi/task-1.py
+++
@@ -1,5 +0,0 @@
-#2
-force = float(input("Введите силу в Н "))
-acceleration = float(input("Введите ускорение в м/с2 " ))
-mass = force/acceleration 
-print(f'Масса : {mass}')
---
+++ nuclear.py
@@ -0,0 +1,5 @@
+#2
+diss = float(input("Введите расстояние в км "))
+time = float(input("Введите время в часах " ))
+velocity = diss/time
+print(f'Скорость : {velocity}')

2560528c70a8f11e52fc4df249cd1a61f03523e3
Anastasia <nastya_romanova-2019@bk.ru>
2024-10-20 16:48:33 +0300

    task-2 commit

--- folder_mephi/task-1.py
+++ folder_mephi/task-1.py
@@ -1,5 +1,5 @@
-#1
-diss_km = float(input("Введите расстояние в км "))
-time = float(input("Введите время в часах " ))
-velocity = diss_km/time 
-print(f'Скорость : {velocity}')
+#2
+force = float(input("Введите силу в Н "))
+acceleration = float(input("Введите ускорение в м/с2 " ))
+mass = force/acceleration 
+print(f'Масса : {mass}')
